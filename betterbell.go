bdd9baaddf883dd6dc8e1e3ec9832da5c403e2fb
logo <logo4poop@protonmail.com>
2023-11-28 22:26:53 -0500

    Play beep

--- betterbell.go
+++ betterbell.go
@@ -42,10 +42,6 @@ func main() {
     }
 }
 
-type name struct {
-    Name string
-}
-
 func getRoot(w http.ResponseWriter, r *http.Request) {
     tpl.ExecuteTemplate(w, "index.html", cfg)
 }
@@ -65,6 +61,7 @@ func postSelectDevice(w http.ResponseWriter, r *http.Request) {
             }
         }
     }
+    beepDevice(cfg.Output)
 }
 
 func playbackDevice() ([]*alsa.Device, error) {
@@ -92,4 +89,124 @@ func playbackDevice() ([]*alsa.Device, error) {
     }
 
     return playback_device, nil
+}
+
+func beepDevice(device *alsa.Device) error {
+	var err error
+
+	if err = device.Open(); err != nil {
+		return err
+	}
+
+	// Cleanup device when done or force cleanup after 3 seconds.
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	defer wg.Wait()
+	childCtx, cancel := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	defer cancel()
+	go func(ctx context.Context) {
+		defer device.Close()
+		<-ctx.Done()
+		log.Println("Closing device.")
+		wg.Done()
+	}(childCtx)
+
+	channels, err := device.NegotiateChannels(1, 2)
+	if err != nil {
+		return err
+	}
+
+	rate, err := device.NegotiateRate(44100)
+	if err != nil {
+		return err
+	}
+
+	format, err := device.NegotiateFormat(alsa.S16_LE, alsa.S32_LE)
+	if err != nil {
+		return err
+	}
+
+	// A 50ms period is a sensible value to test low-ish latency.
+	// We adjust the buffer so it's of minimal size (period * 2) since it appear ALSA won't
+	// start playback until the buffer has been filled to a certain degree and the automatic
+	// buffer size can be quite large.
+	// Some devices only accept even periods while others want powers of 2.
+	wantPeriodSize := 2048 // 46ms @ 44100Hz
+
+	periodSize, err := device.NegotiatePeriodSize(wantPeriodSize)
+	if err != nil {
+		return err
+	}
+
+	bufferSize, err := device.NegotiateBufferSize(wantPeriodSize * 2)
+	if err != nil {
+		return err
+	}
+
+	if err = device.Prepare(); err != nil {
+		return err
+	}
+
+	log.Printf("Negotiated parameters: %d channels, %d hz, %v, %d period size, %d buffer size\n",
+		channels, rate, format, periodSize, bufferSize)
+
+	dec, err := mp3.NewDecoder(f)
+	if err != nil {
+		return fmt.Errorf("could not decode %s: %s", filename, err.Error())
+	}
+
+    
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("could not open %s: %s", filename, err.Error())
+	}
+
+	defer f.Close()
+	var buf bytes.Buffer
+	dec, err := mp3.NewDecoder(f)
+	if err != nil {
+		return fmt.Errorf("could not decode %s: %s", filename, err.Error())
+	}
+
+	// Play 2 seconds of beep.
+	duration := 2 * time.Second
+	t := time.NewTimer(duration)
+	for t := 0.; t < duration.Seconds(); {
+		var buf bytes.Buffer
+
+		for i := 0; i < periodSize; i++ {
+			v := math.Sin(t * 2 * math.Pi * 440) // A4
+			v *= 0.1                             // make a little quieter
+
+			switch format {
+			case alsa.S16_LE:
+				sample := int16(v * math.MaxInt16)
+
+				for c := 0; c < channels; c++ {
+					binary.Write(&buf, binary.LittleEndian, sample)
+				}
+
+			case alsa.S32_LE:
+				sample := int32(v * math.MaxInt32)
+
+				for c := 0; c < channels; c++ {
+					binary.Write(&buf, binary.LittleEndian, sample)
+				}
+
+			default:
+                log.Fatalf("Unhandled sample format: %v", format)
+			}
+
+			t += 1 / float64(rate)
+		}
+
+		if err := device.Write(buf.Bytes(), periodSize); err != nil {
+			return err
+		}
+	}
+	// Wait for playback to complete.
+	<-t.C
+	log.Printf("Playback should be complete now.\n")
+
+	return nil
 }
